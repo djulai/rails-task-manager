4d9d5474dd7f2748ff7838fd61b21283cd190a4a
julie filstroff <julie.filstroff@gmail.com>
2022-11-22 13:21:24 +0100

    tasks

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/tasks", to: "tasks#index", as: :tasks
+  post "tasks", to: "tasks#create"
+  get "/tasks/new", to: "tasks#new", as: :new
+  get "/tasks/:id", to: "tasks#show", as: :id
 end
